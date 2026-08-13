e0db83fccd113e0fcfad385f546f829ea33fb3d4
Gustavo Henrique <gh33493@gmail.com>
2026-08-13 13:41:39 +0000

    adição de mais links

--- pwe01/app/page.js
+++ pwe01/app/page.js
@@ -8,10 +8,25 @@ export default function Home() {
           <h1>
             Aula de Programação Web e Mobile PWE01
           </h1>
-          <h2>Links</h2>
+          <h2>Links Úteis</h2>
           <h3>
             <ul>
             <l1><a href="https://react.dev/" target="_blank"> React</a></l1>
+            <l1><a href="https://nextjs.org/" target="_blank"> NextJS</a></l1>
+            <l1><a href="https://github.com/marciobueno1/pwm-2026-2" target="_blank"></a></l1>
+
+            </ul>
+          </h3>
+          <h2> Slides HTML CSS JS</h2>
+          <h3>
+            <ul>
+              <l1>
+                <a href="https://christianosantos.com.br/files/cs1/construcao-de-sites-1.12.pdf" target="_blank"> Slide 1</a>
+              </l1>
+              <l1> 
+                <a href="https://christianosantos.com.br/files/cs2/construcao-de-sites-2.10.pdf" target="_blank"> Slide 2</a>
+              </l1>
+
             </ul>
           </h3>
          
