824e80a98887b3be833d9981a704e91a43badda3
Gustavo Henrique <gh33493@gmail.com>
2026-08-13 13:27:23 +0000

    adição do link

--- pwe01/app/page.js
+++ pwe01/app/page.js
@@ -8,6 +8,7 @@ export default function Home() {
           <h1>
             Aula de Programação Web e Mobile PWE01
           </h1>
+          <a href="https://react.dev/"> Link para site oficial do react</a>
          
       </main>
     </div>
