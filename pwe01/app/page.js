0edc3991118d59b625f8cef1b8b4223bf1ef2579
Gustavo Henrique <gh33493@gmail.com>
2026-08-13 13:45:00 +0000

    melhoria

--- pwe01/app/page.js
+++ pwe01/app/page.js
@@ -10,24 +10,14 @@ export default function Home() {
           </h1>
           <h2>Links Úteis</h2>
           <h3>
-            <ul>
-            <l1><a href="https://react.dev/" target="_blank"> React</a></l1>
-            <l1><a href="https://nextjs.org/" target="_blank"> NextJS</a></l1>
-            <l1><a href="https://github.com/marciobueno1/pwm-2026-2" target="_blank"></a></l1>
-
-            </ul>
+            <p><a href="https://react.dev/" target="_blank"> React</a></p>
+            <p><a href="https://nextjs.org/" target="_blank"> NextJS</a></p>
+            <p><a href="https://github.com/marciobueno1/pwm-2026-2" target="_blank"></a></p>
           </h3>
           <h2> Slides HTML CSS JS</h2>
-          <h3>
-            <ul>
-              <l1>
-                <a href="https://christianosantos.com.br/files/cs1/construcao-de-sites-1.12.pdf" target="_blank"> Slide 1</a>
-              </l1>
-              <l1> 
-                <a href="https://christianosantos.com.br/files/cs2/construcao-de-sites-2.10.pdf" target="_blank"> Slide 2</a>
-              </l1>
-
-            </ul>
+          <h3>  
+            <p><a href="https://christianosantos.com.br/files/cs1/construcao-de-sites-1.12.pdf" target="_blank"> Slide 1</a></p> 
+            <p><a href="https://christianosantos.com.br/files/cs2/construcao-de-sites-2.10.pdf" target="_blank"> Slide 2</a></p>
           </h3>
          
       </main>
