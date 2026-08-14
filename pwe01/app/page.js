727e79b99d299c654e8ace8845cf4b8f46b15502
Gustavo Henrique <gh33493@gmail.com>
2026-08-14 08:55:15 -0300

    Aula dois tstando tags

--- pwe01/app/page.js
+++ pwe01/app/page.js
@@ -8,6 +8,31 @@ export default function Home() {
           <h1>
             Aula de Programação Web e Mobile PWE01
           </h1>
+          <br />
+          <img src ="images.jpeg" width ="300" height="200"></img>
+          <br />
+          <p><b>Lorem</b> Ipsum é simplesmente uma simulação de texto da indústria <i>tipográfica</i> e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado.Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+          <br />
+          <hr />
+          <p>Lorem <strong>Ipsum</strong> é simplesmente uma simulação de texto da <em>indústria</em> tipográfica e de impressos, e vem sendo utilizado desde o <u></u>, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado.</p>
+          <br />
+          <table border ="1">
+            <thead>
+            <tr>
+              <th>Aluno</th>
+              <th>Nota</th>
+            
+            </tr>
+            </thead>
+            <tbody>
+               <tr>
+                <td>Gustavo</td>
+                <td>10</td>
+               </tr>
+
+            </tbody>
+            
+          </table>
      
       </main>
     </div>
