7b12aa505976e7c8698d167b2ea4764b60e83fe0
Gustavo Henrique <gh33493@gmail.com>
2026-08-26 16:39:37 -0300

    correção no import

--- atividade-01/app/Atividade_246/page.js
+++ atividade-01/app/Atividade_246/page.js
@@ -1,9 +1,9 @@
-import style from "./page.module.css";
+import styles from "./page.module.css";
 
 export default function At246() {
   return (
     <main>
-      <p className={style.paragrafo01}>
+      <p className={styles.paragrafo01}>
         Lorem ipsum dolor sit amet. Ut nesciunt fuga et sapiente autem rem
         doloribus saepe et mollitia dolores ut debitis blanditiis. Cum laborum
         consequatur qui voluptas placeat et veniam officiis qui magni atque qui
@@ -12,8 +12,8 @@ export default function At246() {
         voluptatem culpa qui nobis dolore nam doloremque expedita ut repudiandae
         fugit ab architecto repudiandae eos aperiam aliquid.
       </p>
-      <a href ="https://www.google.com/" className={style.linkM} target="_blank">google </a><br/>
-      <a href ="https://teams.cloud.microsoft/" className={style.linkM} target="_blank">teams</a>
+      <a href ="https://www.google.com/" className={styles.linkM} target="_blank">google </a><br/>
+      <a href ="https://teams.cloud.microsoft/" className={styles.linkM} target="_blank">teams</a>
     </main>
   );
 }
