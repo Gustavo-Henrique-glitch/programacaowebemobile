ac5d7e7bf9cf1a50616e9630ca10fe71a7eafaf0
Gustavo Henrique <gh33493@gmail.com>
2026-08-26 16:31:41 -0300

    conclusão da atividade da pagina 246

--- atividade-01/app/Atividade_246/page.js
+++ atividade-01/app/Atividade_246/page.js
@@ -1,3 +1,19 @@
-export default function At246(){
-    return <p>Pagina 246</p>;
+import style from "./page.module.css";
+
+export default function At246() {
+  return (
+    <main>
+      <p className={style.paragrafo01}>
+        Lorem ipsum dolor sit amet. Ut nesciunt fuga et sapiente autem rem
+        doloribus saepe et mollitia dolores ut debitis blanditiis. Cum laborum
+        consequatur qui voluptas placeat et veniam officiis qui magni atque qui
+        maiores laudantium aut earum blanditiis. A nostrum voluptate sed
+        voluptate eius ut iusto enim nam aliquam labore sed corrupti dolor. Quo
+        voluptatem culpa qui nobis dolore nam doloremque expedita ut repudiandae
+        fugit ab architecto repudiandae eos aperiam aliquid.
+      </p>
+      <a href ="https://www.google.com/" className={style.linkM} target="_blank">google </a><br/>
+      <a href ="https://teams.cloud.microsoft/" className={style.linkM} target="_blank">teams</a>
+    </main>
+  );
 }
